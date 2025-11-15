56aed081a6a5dd6b789b08fab500629ab89023c1
0xKRYNTEX <ricardo50564211@gmail.com>
2025-11-15 02:52:04 -0300

    Add service worker for caching assets

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'kryntex-cache-v1';
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/script.js',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+self.addEventListener('install', evt => {
+  evt.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', evt => {
+  evt.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(resp => resp || fetch(evt.request))
+  );
+});
